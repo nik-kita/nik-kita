bb16e9e08ca073b43446878a58a77132e40fa415
nikita <nik.moiseienko@gmail.com>
2023-12-18 09:05:20 +0100

    mvp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
+import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import Heading from "@theme/Heading";
-import Link from "@docusaurus/Link";
 
-import HomepageHeader from "./_home/home-page-header";
 import { tw } from "../utils/tw";
+import HomepageHeader from "./_home/home-page-header";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
