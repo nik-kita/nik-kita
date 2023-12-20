cdd7df5d036d1758d93834fff634961ce92acf1e
nikita <nik.moiseienko@gmail.com>
2023-12-20 01:08:54 +0100

    add utils/types

---
+++ src/utils/types.ts
@@ -0,0 +1,16 @@
+export type OmitStrict<
+  T extends Record<string, any>,
+  K extends keyof T,
+> = Omit<T, K>;
+
+export type OmitReplace<
+  T extends Record<string, any>,
+  U extends Record<string, any>,
+> = keyof U extends (infer R extends keyof T)
+  ? Omit<T, R> & U
+  : never;
+
+export type AddOrReplace<
+  T extends Record<string, any>,
+  U extends Record<string, any>
+> = Omit<T, keyof U> & U;
